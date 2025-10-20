302a05c80c232e4c9d5f2696a00bcc945315ca88
Kentzie <onixd502@gmail.com>
2025-10-20 19:14:26 +0700

    beres semua

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,92 +1,151 @@
-// ignore_for_file: dead_code, prefer_const_constructors
+// ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
-import 'package:uts_2022130009/screens/cart_screen.dart';
+import 'package:uts_2022130009/screens/login_screen.dart';
 import 'package:uts_2022130009/screens/product_screen.dart';
+import 'package:uts_2022130009/screens/cart_screen.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    final products = [
-      {
-        'name': 'Sneakers',
-        'price': 'Rp 350.000',
-        'image': 'https://picsum.photos/200'
-      },
-      {
-        'name': 'T-Shirt',
-        'price': 'Rp 120.000',
-        'image': 'https://picsum.photos/201'
-      },
-      {
-        'name': 'Smart Watch',
-        'price': 'Rp 500.000',
-        'image': 'https://picsum.photos/202'
-      },
-      {
-        'name': 'Polo Shirt',
-        'price': 'Rp 270.000',
-        'image': 'https://picsum.photos/203'
-      },
-      {
-        'name': 'Hat',
-        'price': 'Rp 50.000',
-        'image': 'https://picsum.photos/204'
-      },
-      {
-        'name': 'Leather Shoes',
-        'price': 'Rp 500.000',
-        'image': 'https://picsum.photos/205'
-      },
-      {
-        'name': 'Wrist Band',
-        'price': 'Rp 15.000',
-        'image': 'https://picsum.photos/206'
-      },
-      {
-        'name': 'Sun Glasses',
-        'price': 'Rp 100.000',
-        'image': 'https://picsum.photos/207'
-      },
-    ];
+  State<HomeScreen> createState() => _HomeScreenState();
+}
 
+class _HomeScreenState extends State<HomeScreen> {
+  List<Map<String, dynamic>> cartItems = [];
+
+  final products = [
+    {
+      'name': 'Sneakers',
+      'price': 'Rp 350.000',
+      'image': 'https://picsum.photos/200'
+    },
+    {
+      'name': 'T-Shirt',
+      'price': 'Rp 120.000',
+      'image': 'https://picsum.photos/201'
+    },
+    {
+      'name': 'Smart Watch',
+      'price': 'Rp 500.000',
+      'image': 'https://picsum.photos/202'
+    },
+    {
+      'name': 'Polo Shirt',
+      'price': 'Rp 270.000',
+      'image': 'https://picsum.photos/203'
+    },
+    {'name': 'Hat', 'price': 'Rp 50.000', 'image': 'https://picsum.photos/204'},
+    {
+      'name': 'Leather Shoes',
+      'price': 'Rp 500.000',
+      'image': 'https://picsum.photos/205'
+    },
+    {
+      'name': 'Wrist Band',
+      'price': 'Rp 15.000',
+      'image': 'https://picsum.photos/206'
+    },
+    {
+      'name': 'Sun Glasses',
+      'price': 'Rp 100.000',
+      'image': 'https://picsum.photos/207'
+    },
+  ];
+
+  void _goToCart() {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => CartScreen(cartItems: cartItems),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.blueAccent,
-        elevation: 09,
-        title: Text(
-          'BShop',
-          style: TextStyle(fontWeight: FontWeight.bold),
-        ),
+        title:
+            const Text('BShop', style: TextStyle(fontWeight: FontWeight.bold)),
         actions: [
           IconButton(
-            icon: Icon(Icons.shopping_cart),
-            onPressed: () {
-              // Aksi ketika ikon keranjang ditekan
-            },
+            icon: const Icon(Icons.shopping_cart),
+            onPressed: _goToCart,
           ),
         ],
       ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.blueAccent,
+              ),
+              child: Text(
+                'Menu',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            ListTile(
+                leading: Icon(Icons.home),
+                title: Text('Logout'),
+                onTap: () {
+                  Navigator.pop(context);
+                  showDialog(
+                    context: context,
+                    builder: (context) => AlertDialog(
+                      title: Text('Logout'),
+                      content: Text('Are you sure you want to logout?'),
+                      actions: [
+                        TextButton(
+                          onPressed: () => Navigator.pop(context),
+                          child: Text('Cancel'),
+                        ),
+                        TextButton(
+                          onPressed: () {
+                            Navigator.pop(context);
+                            Navigator.pushAndRemoveUntil(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => LoginScreen()
+                              ),
+                              (route) => false,
+                            );
+                          },
+                          child: Text('Logout'),
+                        ),
+                      ],
+                    ),
+                  );
+                }),
+          ],
+        ),
+      ),
       body: Padding(
-        padding: EdgeInsets.all(16),
+        padding: const EdgeInsets.all(16),
         child: Column(
           children: [
             TextField(
               decoration: InputDecoration(
                 hintText: "Search products...",
-                prefixIcon: Icon(Icons.search),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(12),
-                ),
+                prefixIcon: const Icon(Icons.search),
+                border:
+                    OutlineInputBorder(borderRadius: BorderRadius.circular(12)),
               ),
             ),
-            SizedBox(height: 20),
+            const SizedBox(height: 20),
             Expanded(
               child: GridView.builder(
                 itemCount: products.length,
-                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
                   crossAxisCount: 2,
                   crossAxisSpacing: 12,
                   mainAxisSpacing: 12,
@@ -96,7 +155,7 @@ class HomeScreen extends StatelessWidget {
                   final product = products[index];
                   return GestureDetector(
                     onTap: () async {
-                      Navigator.push(
+                      final updatedCart = await Navigator.push(
                         context,
                         MaterialPageRoute(
                           builder: (context) => ProductScreen(
@@ -113,18 +172,16 @@ class HomeScreen extends StatelessWidget {
                         });
                       }
                     },
-                    },
                     child: Card(
                       shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(12),
-                      ),
+                          borderRadius: BorderRadius.circular(12)),
                       elevation: 2,
                       child: Column(
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
                           ClipRRect(
-                            borderRadius:
-                                BorderRadius.vertical(top: Radius.circular(12)),
+                            borderRadius: const BorderRadius.vertical(
+                                top: Radius.circular(12)),
                             child: Image.network(
                               product['image']!,
                               height: 120,
@@ -133,25 +190,18 @@ class HomeScreen extends StatelessWidget {
                             ),
                           ),
                           Padding(
-                            padding: EdgeInsets.all(8.0),
+                            padding: const EdgeInsets.all(8.0),
                             child: Column(
                               crossAxisAlignment: CrossAxisAlignment.start,
                               children: [
-                                Text(
-                                  product['name']!,
-                                  style: TextStyle(
-                                    fontWeight: FontWeight.bold,
-                                    fontSize: 16,
-                                  ),
-                                ),
-                                SizedBox(height: 4),
-                                Text(
-                                  product['price']!,
-                                  style: TextStyle(
-                                    color: Colors.grey[600],
-                                    fontSize: 14,
-                                  ),
-                                ),
+                                Text(product['name']!,
+                                    style: const TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 16)),
+                                const SizedBox(height: 4),
+                                Text(product['price']!,
+                                    style: TextStyle(
+                                        color: Colors.grey[600], fontSize: 14)),
                               ],
                             ),
                           ),
